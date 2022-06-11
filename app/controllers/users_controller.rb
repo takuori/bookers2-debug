8b6b010fd6700399f7b3e99bad6c3d559623cf23
takuori <104710179+takuori@users.noreply.github.com>
2022-06-11 01:44:31 +0000

    8b過去7日間表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
+    @book = Book.new
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,22 +2,30 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
   has_many :view_counts, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
-  scope :created_today, -> {where(created_at: Time.zone.now.all_day) }
+
+  scope :created_today, ->  {where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
